bd29645e036217383f4b4719979440ba815f3903
Obinna Justice <official.info.obitech@gmail.com>
2025-03-07 21:31:40 +0100

    Update provider.tsx

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,4 +1,19 @@
 "use client";
+"use client";
+"use client";
+"use client";
+"use client";
+"use client";
+"use client";
+"use client";
+"use client";
+"use client";
+"use client";
+"use client";
+"use client";
+"use client";
+"use client";
+"use client";
 
 import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider } from "next-themes";
