5be441f39b802de4d700ec69671271055a80e041
Obinna Justice <official.info.obitech@gmail.com>
2025-04-16 16:06:13 +0100

    Update Footer.jsx

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,7 +2,6 @@ import Link from "next/link";
 import MappedLinks from "./MapedLinks";
 import MappedLinks from "./MapedLinks";
 import MappedLinks from "./MapedLinks";
-import MappedLinks from "./MapedLinks";
 
 
 export default function Footer() {
